d90ad3d9da0ac0128a79e1a4463e7b4ff886295e
Josowsky <bartosz@jozwowiak.pl>
2018-06-09 23:51:30 +0200

    Add print method to Node class

--- utils/Node.py
+++ utils/Node.py
@@ -4,5 +4,14 @@ class Node:
         self.leftChild = leftChild
         self.rightChild = rightChild
     
-    def grantsPermission():
+    def grantsPermissions(self):
         return True
+
+    def printNode(self):
+        leftChild = None
+        rightChild = None
+        if self.leftChild:
+            leftChild = self.leftChild.value
+        if self.rightChild:
+            rightChild = self.rightChild.value
+        print '[{}] <- [{}] -> [{}] \n'.format(leftChild, self.value, rightChild)
