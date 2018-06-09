a3b32b529027b841f63d2f48a708ff1cf7772816
Josowsky <bartosz@jozwowiak.pl>
2018-06-09 23:52:00 +0200

    Update main function

--- pr.py
+++ pr.py
@@ -1,14 +1,19 @@
-import logging # This module is thread safe.
+import logging
 import threading
 import pprint
+
+from utils.printTree import printTree
 from utils.getBinaryTree import getBinaryTree
+from utils.getQuorum import getQuorum
 
 logging.basicConfig(level=logging.INFO)
 LOCK = threading.Lock()
 
-NUMBER_OF_HOSTS = 16
+# Constants
+NUMBER_OF_HOSTS = 10
 
 
+# Main execution function (probably will get depracated with MPI arival)
 def run(arg):
     if LOCK.acquire(False): # Non-blocking -- return whether we got it
         logging.info('Got the lock! {}'.format(arg))
@@ -21,9 +26,16 @@ threads = []
 for i in range(NUMBER_OF_HOSTS):
 	threads.append(threading.Thread(target=run, args=[i]))
 
+# Create binary tree
 binary_tree = getBinaryTree(NUMBER_OF_HOSTS)
 
-print binary_tree
+# Print binary tree
+print '===== GENERATED BINARY TREE ===='
+printTree(binary_tree)
+print '=====   =====   ===== \n\n'
+
+# Get tree structured quorum
+print getQuorum(binary_tree)
 
 # for thread in threads:
 #    thread.start()
