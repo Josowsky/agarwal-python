74a719980957e0054bff351c2a51d599f8b65867
Josowsky <bartosz@jozwowiak.pl>
2018-06-11 00:12:43 +0200

    Implement send and reuqest in mpiInterface

--- utils/mpi.py
+++ utils/mpi.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+import time
 
 MESSAGE_REQUEST = 1
 
@@ -8,7 +9,22 @@ class MpiInterface:
         self.NUMBER_OF_HOSTS = self.COMM.Get_size()
         self.HOST_ID = self.COMM.Get_rank()
 
-    def request(hostId, quorumSet):
-        for host in quorumSet:
-            pass
-            # send request message from sender to receiver
+    def listen(self):
+        print 'Listening to any mesage at {}'.format(self.HOST_ID)
+
+        messageListener = self.COMM.irecv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+        message = messageListener.wait()
+        
+        print 'Received {} from {} to {}'.format(message['tag'], message['senderId'], self.HOST_ID)
+        # print 'Received {} from {} to {}'.format(1, self.HOST_ID) 
+
+    def request(self, senderId, quorumSet):
+        for receiverId in quorumSet:
+            print 'Sending REQUEST from {} to {}'.format(senderId, receiverId)
+            data = {
+                'tag': MESSAGE_REQUEST,
+                'senderId': senderId,
+                'timestamp': time.time()
+            }
+
+            req = self.COMM.isend(data, dest=receiverId, tag=MESSAGE_REQUEST)
