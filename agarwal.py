e87bd3ac303687549600099cdbee50a871f367a8
Josowsky <bartosz@jozwowiak.pl>
2018-06-11 01:34:51 +0200

    Implement reply

--- agarwal.py
+++ agarwal.py
@@ -1,17 +1,20 @@
-from utils.mpi import MpiInterface
+import time
+
+from utils.mpi import MpiInterface as MPI
 from utils.printTree import printTree
 from utils.getBinaryTree import getBinaryTree, getMiddleElement
 from utils.getQuorum import getQuorum
 
 # Initialize mpi
-mpiInterface = MpiInterface()
+mpiInterface = MPI()
 
 # Constants
+REFRESH_INTERVAL = 0.5
 NUMBER_OF_HOSTS = mpiInterface.NUMBER_OF_HOSTS
 HOST_ID = mpiInterface.HOST_ID
 
 # # Create hosts vector
-# hosts = range(0, NUMBER_OF_HOSTS)
+hosts = range(0, NUMBER_OF_HOSTS)
 
 # Put our process in the middle of hosts vector
 middleElementIndex = getMiddleElement(hosts)
@@ -22,16 +25,26 @@ hosts[middleElementIndex] = HOST_ID
 binary_tree = getBinaryTree(hosts)
 
 # Print binary tree
-print '===== GENERATED BINARY TREE ===='
-printTree(binary_tree)
-print '=====   =====   ===== \n\n'
+# print '===== GENERATED BINARY TREE ===='
+# printTree(binary_tree)
+# print '=====   =====   ===== \n\n'
 
 # Get tree structured quorum
-print '===== QUORUM ====='
-print '{}\n'.format(getQuorum(binary_tree))
+quorumSet = getQuorum(binary_tree)
+
+# Prepare set of proces that need to accept request
+replaySet = quorumSet
+replaySet.remove(HOST_ID)
+mpiInterface.quorumSet = replaySet
+
+# Print tree structured quorum
+# print '===== QUORUM ====='
+# print '{}\n'.format(quorumSet)
 
 # Request access to CS
-pass
+if HOST_ID == 1:
+    mpiInterface.request(HOST_ID, quorumSet)
 
-# for thread in threads:
-#    thread.start()
+while True:
+    mpiInterface.listen()
+    time.sleep(REFRESH_INTERVAL)
--- utils/mpi.py
+++ utils/mpi.py
@@ -2,6 +2,7 @@ from mpi4py import MPI
 import time
 
 MESSAGE_REQUEST = 1
+MESSAGE_REPLY = 2
 
 class MpiInterface:
     def __init__(self):
@@ -9,17 +10,63 @@ class MpiInterface:
         self.NUMBER_OF_HOSTS = self.COMM.Get_size()
         self.HOST_ID = self.COMM.Get_rank()
 
-    def listen(self):
-        print 'Listening to any mesage at {}'.format(self.HOST_ID)
+        self.requestQueue = []
+        self.quorumSet = []
 
-        messageListener = self.COMM.irecv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
-        message = messageListener.wait()
-        
-        print 'Received {} from {} to {}'.format(message['tag'], message['senderId'], self.HOST_ID)
-        # print 'Received {} from {} to {}'.format(1, self.HOST_ID) 
+    def addToRequestQueue(self, newRequest):
+        if len(self.requestQueue) == 0:
+            self.requestQueue = [newRequest]
+            return
+
+        for index, request in enumerate(self.requestQueue):
+            if request['timestamp'] > newRequest['timestamp']:
+                self.requestQueue.insert(index, newRequest)
+                break
+            
+            if index == len(self.requestQueue - 1):
+                self.requestQueue.append(newRequest)
+
+    def saveReply(self, reply):
+        replySenderId = reply['senderId']
+        self.quorumSet.remove(replySenderId)
+
+        if len(self.quorumSet) == 0:
+            print 'I can enter the CS!! {}'.format(self.HOST_ID)
+
+    def listen(self):
+        self.reply()
+
+        if self.COMM.iprobe(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG):
+            messageListener = self.COMM.irecv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+            message = messageListener.wait()
+
+            if message['senderId'] == self.HOST_ID:
+                return
+            
+            if message['tag'] == MESSAGE_REQUEST:
+                self.addToRequestQueue(message)
+
+            if message['tag'] == MESSAGE_REPLY:
+                self.saveReply(message)
+
+    def reply(self):
+        if len(self.requestQueue) > 0:
+            replyReceiver = self.requestQueue.pop(0)
+            print 'Reply: {} allows {} to enter CS'.format(self.HOST_ID, replyReceiver['senderId'])
+
+            data = {
+                'tag': MESSAGE_REPLY,
+                'senderId': self.HOST_ID,
+                'timestamp': time.time()
+            }
+
+            self.COMM.isend(data, dest=replyReceiver['senderId'], tag=MESSAGE_REPLY)
 
     def request(self, senderId, quorumSet):
         for receiverId in quorumSet:
+            if receiverId == self.HOST_ID:
+                continue
+
             print 'Sending REQUEST from {} to {}'.format(senderId, receiverId)
             data = {
                 'tag': MESSAGE_REQUEST,
@@ -27,4 +74,4 @@ class MpiInterface:
                 'timestamp': time.time()
             }
 
-            req = self.COMM.isend(data, dest=receiverId, tag=MESSAGE_REQUEST)
+            self.COMM.isend(data, dest=receiverId, tag=MESSAGE_REQUEST)
